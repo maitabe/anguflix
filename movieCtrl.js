8750acd396dc60749c9790260375d2508ddd5138
maitabe <martushb@gmail.com>
2016-11-25 21:43:43 +0200

    remove movie from user collection

--- movieCtrl.js
+++ movieCtrl.js
@@ -9,13 +9,20 @@ app.controller('movieCtrl', function($scope, movieService) {
 	$scope.userMovies = movieService.userMovies;
 
 
-	//invoke function
-	$scope.addUserMovies = function($index) {
-		movieService.addUserMovies($index);
+	//invoke functions
+	//add movie to "My collection section"
+	$scope.addUserMovies = function() {
+		movieService.addUserMovies(this.movie);
 	};
 
-	// $scope.removeMovie = function($index) {
-	// 	movieService.removeMovie($index);
-	// };
+	//remove movie from "my collection"
+	$scope.removeMovieMyCollection = function() {
+		movieService.removeMovie(this.$index);
+	};
+
+	//search movie by name
+	$scope.findit = function(){
+		$scope.searchResults = $scope.search;
+	};
 
 });
--- movieService.js
+++ movieService.js
@@ -3,6 +3,7 @@ app.factory('movieService', function() {
 
 	var allMovies = [
 		{
+			id:1,
 			name: 'Spirited Away',
 			director: 'Hayao Miyazaki',
 			year: 2001,
@@ -12,6 +13,7 @@ app.factory('movieService', function() {
 			description:'During her family&apos;s move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.'
 		},
 		{
+			id:2,
 			name: 'Bridge of Spies',
 			director: 'Steven Spielberg',
 			year: 2015,
@@ -21,6 +23,7 @@ app.factory('movieService', function() {
 			description:'During the Cold War, an American lawyer is recruited to defend an arrested Soviet spy in court, and then help the CIA facilitate an exchange of the spy for the Soviet captured American U2 spy plane pilot, Francis Gary Powers.'
 		},
 		{
+			id:3,
 			name: 'Interstellar',
 			director: 'Christopher Nolan',
 			year: 2014,
@@ -30,27 +33,30 @@ app.factory('movieService', function() {
 			description:'A team of explorers travel through a wormhole in space in an attempt to ensure humanitys survival.'
 		},
 		{
-			name: 'Interstellar',
+			id:4,
+			name: 'Interstellar 2',
 			director: 'Christopher Nolan',
-			year: 2014,
+			year: 1989,
 			image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_UY1200_CR69,0,630,1200_AL_.jpg',
 			rating: 8.7,
 			price: 24,
 			description:'A team of explorers travel through a wormhole in space in an attempt to ensure humanitys survival.'
 		},
 		{
-			name: 'Spirited Away',
+			id:5,
+			name: 'Spirited Away 2',
 			director: 'Hayao Miyazaki',
-			year: 2001,
+			year: 2010,
 			image: 'http://t1.gstatic.com/images?q=tbn:ANd9GcS6MveoDoJOg9-wMvtHE4ak_-HDZeYS1egb9PwRcf8lhrtcppMc',
 			rating: 8.6,
 			price: 25,
 			description:'During her family&apos;s move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.'
 		},
 		{
-			name: 'Bridge of Spies',
+			id:6,
+			name: 'Bridge of Spies 2',
 			director: 'Steven Spielberg',
-			year: 2015,
+			year: 1999,
 			image: 'http://www.impawards.com/2015/posters/bridge_of_spies_ver2_xxlg.jpg',
 			rating: 7.6,
 			price: 20,
@@ -60,20 +66,29 @@ app.factory('movieService', function() {
 
 	userMovies = [];
 
-	var addUserMovies = function($index) {
-		if(!userMovies[$index]){
-			userMovies.push(allMovies[$index]);
+
+	var addUserMovies = function(selectedMovie) {
+		//find the id of the movie selected
+		var result = userMovies.find(function(movie) {
+			//testing function(the anonymous func): will give me back the object(value) of the movie
+			return movie.id === selectedMovie.id;
+		});
+		//check if user collection has selected movie
+		if(result === undefined){
+			// not found in collection
+			userMovies.push(selectedMovie);
 		}
 	};
 
-	// var removeMovie = function() {
-	// 	userMovies.splice($index, 1);
-	// };
+	var removeMovie = function(index) {
+		userMovies.splice(index, 1);
+	};
 
 	return {
 		allMovies:allMovies,
 		userMovies:userMovies,
-		addUserMovies:addUserMovies
+		addUserMovies:addUserMovies,
+		removeMovie:removeMovie
 	};
 
 });
